2ec3c1681f21c34b2b17cfa5d928ce5f708627d4
Brian <42729519@fi.unju.edu.ar>
2025-04-24 12:04:33 -0300

    se actualizó el puerto y CORS

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+  });
   app.setGlobalPrefix('api');
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();

e7973183483c6408ab42b3776530f6a37f013264
Brian <42729519@fi.unju.edu.ar>
2025-07-23 09:52:06 -0300

    aaaah

--- src/progreso/progreso.module.ts
+++ src/progreso/progreso.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProgresoService } from './progreso.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { PlanillasModule } from '../planillas/planillas.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, PlanillasModule],
   providers: [ProgresoService],
   exports: [ProgresoService], 
 })
